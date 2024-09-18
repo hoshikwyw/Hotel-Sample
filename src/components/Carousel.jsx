9cec68b07d2aaa607e647a487f33dc635449b778
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-09-18 13:05:59 +0630

    Giving wrong id

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,48 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-const Carousel = ({data}) => {
-    console.log(data?.roomImgs);
-    
+const Carousel = ({ data }) => {
+    // console.log(data);
+
+    const roomPhotos = data
+
     return (
         <div className="carousel w-full">
-            <div id="slide1" className="carousel-item relative w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-                    className="w-full" />
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a>
+            {roomPhotos?.map((img, index) => (
+                <div key={index} id={`slide${index + 1}`} className="carousel-item relative w-full">
+                    <img src={img} className=" skeleton w-full h-96" alt={`Slide ${index + 1}`} />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        {/* Previous button */}
+                        <a
+                            href={`#slide${(index === 0 ? roomPhotos.length - 1 : index - 1) + 1}`}
+                            className="btn btn-circle"
+                        >
+                            ❮
+                        </a>
+                        {/* Next button */}
+                        <a
+                            href={`#slide${(index === roomPhotos.length - 1 ? 0 : index + 1) + 1}`}
+                            className="btn btn-circle"
+                        >
+                            ❯
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div id="slide2" className="carousel-item relative w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-                    className="w-full" />
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide1" className="btn btn-circle">❮</a>
-                    <a href="#slide3" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide3" className="carousel-item relative w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-                    className="w-full" />
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide2" className="btn btn-circle">❮</a>
-                    <a href="#slide4" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-                    className="w-full" />
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
-                    <a href="#slide1" className="btn btn-circle">❯</a>
-                </div>
-            </div>
+            ))}
         </div>
-    )
-}
+    );
+};
 
-export default Carousel
+export default Carousel;
